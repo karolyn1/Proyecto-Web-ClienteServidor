3d2d57601a440b9e803f7ad743cf51b0ffb812ed
arianafc <arianafc1108@gmail.com>
2024-11-09 22:41:13 -0600

    update

--- vistaDetalleAnimal.php
+++ vistaDetalleAnimal.php
@@ -10,22 +10,14 @@
     <link href="https://fonts.googleapis.com/css2?family=Paytone+One&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
-
-
 <body>
 
-<?php
-include("fragmentos.php");
-echo $navbar;
-?>
 
-    <header class="detalleAnimal-title text-center">
-        <h1 class="card-title">PERFILES DE NUESTROS ANIMALES</h1>
-    </header>
-
-
-
-
+<main>
+    <div class="detalleAnimal-title text-center">
+        <h1 >PERFILES DE NUESTROS ANIMALES</h1>
+</div>
+</main>
 
 </body>
 </html>
